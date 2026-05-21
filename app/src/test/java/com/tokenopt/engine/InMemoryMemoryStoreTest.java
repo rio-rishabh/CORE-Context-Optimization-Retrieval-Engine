e577667916b00d89ffa03b389107e85a40258b65
Rishabh Sharma <riorishabh448@gmail.com>
2026-05-20 23:05:50 -0400

    adding InMemoryMemoryStoreTest

---
+++ app/src/test/java/com/tokenopt/engine/InMemoryMemoryStoreTest.java
@@ -0,0 +1,10 @@
+package com.tokenopt.engine;
+
+import org.junit.jupiter.api.Test;
+
+public class InMemoryMemoryStoreTest {
+
+    @Test
+    public void searchTopK_returnsClosestMatchFirst(){
+    }
+}
